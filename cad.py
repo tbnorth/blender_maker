df759c5c8c9f88406c52df9ab974c82a7d9ee047
Terry Brown <terrynbrown@gmail.com>
2018-11-21 22:16:46 -0600

    fix def order, run black

--- cad.py
+++ cad.py
@@ -13,6 +13,13 @@ bpyscene = bpy.context.scene
 
 FINAL = True  # use slower code to get details right
 
+
+def _v(*args):
+    if len(args) == 1:
+        return Vector(*args)
+    return Vector(args)
+
+
 OREL = {
     # order these "clockwise from above" (or front, or right) for convenient
     # placement of parts at an angle, which will be rotated 90 degrees between
@@ -26,12 +33,7 @@ OREL = {
 for x, rx in (('l', 0), ('c', 0.5), ('r', 1)):
     for y, ry in (('f', 0), ('c', 0.5), ('b', 1)):
         for z, rz in (('b', 0), ('c', 0.5), ('t', 1)):
-            OREL[x+y+z] = _v(rx, ry, rz)
-
-def _v(*args):
-    if len(args) == 1:
-        return Vector(*args)
-    return Vector(args)
+            OREL[x + y + z] = _v(rx, ry, rz)
 
 
 def vmult(a, b):
@@ -54,7 +56,8 @@ def rel_coords(obj, orel):
         orel = [orel]
 
     ans = [
-        obj.matrix_world * _v(
+        obj.matrix_world
+        * _v(
             bbox[0] + (bbox[3] - bbox[0]) * i[0],
             bbox[1] + (bbox[4] - bbox[1]) * i[1],
             bbox[2] + (bbox[5] - bbox[2]) * i[2],
@@ -95,16 +98,18 @@ def new_obj(name=None):
 
 def obj_add(obj, what='cube', **kwargs):
     if what == 'unit_cyl':
-        return obj_add(obj, what='cone',
+        return obj_add(
+            obj,
+            what='cone',
             diameter1=0.5,
             diameter2=0.5,
             depth=1,
             segments=40,
-            cap_ends=True
-                       )
+            cap_ends=True,
+        )
     bm = bmesh.new()
     bm.from_mesh(obj.data)
-    getattr(bmesh.ops, 'create_'+what)(bm, **kwargs)
+    getattr(bmesh.ops, 'create_' + what)(bm, **kwargs)
     # bmesh.ops.create_cube(bm, size=1.0)
     bm.to_mesh(obj.data)
     bm.free()
@@ -219,6 +224,7 @@ def origin(obj, pos=None):
     bm.to_mesh(obj.data)
     bm.free()
 
+
 def crange(obj, start, end, steps):
     ans = []
     start = rel_coords(obj, start)
@@ -227,12 +233,15 @@ def crange(obj, start, end, steps):
     for i in range(steps[0]):
         for j in range(steps[1]):
             for k in range(steps[2]):
-                ans.append(_v(
-                    start[0]+(end[0]-start[0])*(i/max(1, steps[0]-1)),
-                    start[1]+(end[1]-start[1])*(j/max(1, steps[1]-1)),
-                    start[2]+(end[2]-start[2])*(k/max(1, steps[2]-1)),
-                ))
+                ans.append(
+                    _v(
+                        start[0] + (end[0] - start[0]) * (i / max(1, steps[0] - 1)),
+                        start[1] + (end[1] - start[1]) * (j / max(1, steps[1] - 1)),
+                        start[2] + (end[2] - start[2]) * (k / max(1, steps[2] - 1)),
+                    )
+                )
     return ans
+
 
 reset_blend()
 
@@ -248,17 +257,19 @@ drill = replicate(cyl)
 size(drill, (2, 2, 2))
 do_bool(cyl, drill, 'DIFFERENCE')
 
-size(drill, (.75, .75, 2))  # to drill holes around the edge
+size(drill, (0.75, 0.75, 2))  # to drill holes around the edge
 # rather than diffing out each hole one at a time, merge all holes
 # into `alt` and diff all at once, much cleaner mesh
 alt = new_obj()
-for coord in crange(pyb, (.03, .03, .5), (.97, .97, .5), (2, 16, 1)):
+for coord in crange(pyb, (0.03, 0.03, 0.5), (0.97, 0.97, 0.5), (2, 16, 1)):
     move_to(drill, coord)
     if FINAL:  # slow
         do_bool(alt, drill, "UNION")
     else:
         replicate(drill, "tmp_hole")
-for n, coord in enumerate(crange(pyb, (.03, .97, .5), (.97, .97, .5), (12, 1, 1))):
+for n, coord in enumerate(
+    crange(pyb, (0.03, 0.97, 0.5), (0.97, 0.97, 0.5), (12, 1, 1))
+):
     if n in (0, 11):
         continue
     move_to(drill, coord)
@@ -277,7 +288,7 @@ size(chip, (11, 11, 1))
 origin(chip, 'ccb')
 move_to(chip, rel_coords(pyb, 'cct'))
 base = replicate(chip, "reset")
-move_to(base, rel_coords(pyb, (.25, .25, 1)))
+move_to(base, rel_coords(pyb, (0.25, 0.25, 1)))
 size(base, (3, 2, 1))
 knob = replicate(base)
 size(knob, (1.2, 1, 1))
@@ -285,8 +296,8 @@ move_to(knob, rel_coords(base, 'cct'))
 do_bool(base, knob, "UNION")
 delete(knob)
 base = replicate(base, "user")
-move_to(base, rel_coords(pyb, (.45, .25, 1)))
-for n, coord in enumerate(crange(pyb, (.375, 0.02, 1), (.375, 0.2, 1), (1, 4, 1))):
+move_to(base, rel_coords(pyb, (0.45, 0.25, 1)))
+for n, coord in enumerate(crange(pyb, (0.375, 0.02, 1), (0.375, 0.2, 1), (1, 4, 1))):
     led = replicate(chip, "LED")
     if n >= 2:
         rotate(led, (0, 0, 90))
@@ -296,14 +307,14 @@ for n, coord in enumerate(crange(pyb, (.375, 0.02, 1), (.375, 0.2, 1), (1, 4, 1)
 # with cfb origin
 chip = replicate(chip, "USB")
 origin(chip, 'cfb')
-move_to(chip, rel_coords(pyb, (.2, 0, 1)))
+move_to(chip, rel_coords(pyb, (0.2, 0, 1)))
 size(chip, (6, 4, 2))
 chip = replicate(chip, "uSD")
-move_to(chip, rel_coords(pyb, (.6, 0, 1)))
+move_to(chip, rel_coords(pyb, (0.6, 0, 1)))
 size(chip, (11, 5, 2))
 
 # now add the ~toroid mount points
-origin(cyl, OREL['lcc']+_v(0.1, 0, 0))
+origin(cyl, OREL['lcc'] + _v(0.1, 0, 0))
 move_to(cyl, rel_coords(pyb, (1, 0.05, 0.5)))
 do_bool(pyb, cyl, 'UNION')
 rotate(cyl, (0, 0, 180))
@@ -321,7 +332,7 @@ size(cap, (7, 7, 10))
 origin(cap, 'ccb')
 move_to(cap, rel_coords(cond, (0.35, 0.225, 1)))
 cap = replicate(cap)
-scale(cap, (.65, .65, .5))
+scale(cap, (0.65, 0.65, 0.5))
 move_to(cap, rel_coords(cond, (0.3, 0.12, 1)))
 
 alt = new_obj()
@@ -329,7 +340,7 @@ size(drill, (3, 3, 3))
 for coord in crange(cond, (0.075, 0.05, 0.5), (0.925, 0.95, 0.5), (2, 2, 1)):
     move_to(drill, coord)
     do_bool(alt, drill, "UNION")
-size(drill, (.75, .75, 3))
+size(drill, (0.75, 0.75, 3))
 for coord in crange(cond, (0.925, 0.35, 0.5), (0.925, 0.45, 0.5), (1, 3, 1)):
     move_to(drill, coord)
     do_bool(alt, drill, "UNION")
@@ -343,7 +354,7 @@ switch = new_obj("switch")
 obj_add(switch)
 size(switch, (5, 4, 2))
 origin(switch, 'ccb')
-move_to(switch, rel_coords(cond, (.15, .825, 1)))
+move_to(switch, rel_coords(cond, (0.15, 0.825, 1)))
 knob = replicate(switch)
 size(knob, (1, 0.5, 0.7))
 for coord in crange(switch, (0.3, 0.3, 1), (0.3, 0.7, 1), (1, 2, 1)):
@@ -366,7 +377,7 @@ size(drill, (3, 3, 3))
 for coord in crange(gps, (0.07, 0.1, 0.5), (0.07, 0.9, 0.5), (1, 2, 1)):
     move_to(drill, coord)
     do_bool(alt, drill, "UNION")
-size(drill, (.75, .75, 3))
+size(drill, (0.75, 0.75, 3))
 for coord in crange(gps, (0.95, 0.05, 0.5), (0.95, 0.95, 0.5), (1, 9, 1)):
     move_to(drill, coord)
     do_bool(alt, drill, "UNION")
